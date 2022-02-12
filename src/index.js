20b7dd3c3741fb7f89d933dc0d69d4cf34e54677
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-02-12 13:26:58 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './assets/index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
