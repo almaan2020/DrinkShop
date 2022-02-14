5c15e5c343784ad783386ba1a36dfe0d86a0c0e8
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-02-14 18:54:56 +0330

    Update Pagination.jsx

--- src/components/shop/Pagination.jsx
+++ src/components/shop/Pagination.jsx
@@ -4,26 +4,39 @@ import { ProductContext } from "../../contexts/ProductContext";
 import paginations from "../../config/pagination.json";
 
 const Pagination = () => {
-  const { currentPage, handlePage, query } = useContext(ProductContext);
-  const { countPerPage, allCount } = paginations;
+  const { searchParams, handleSearch } = useContext(ProductContext);
+  const {
+    page: currentPage,
+    query: currentQuery,
+    sort: currentSort,
+    order: currentOrder,
+  } = Object.fromEntries([...searchParams]);
 
-  const itemCounts = query ? countPerPage : allCount;
+  const { countPerPage, allCount } = paginations;
+  const itemCounts = currentQuery ? countPerPage : allCount;
   const pageCount = Math.ceil(itemCounts / countPerPage);
 
   if (pageCount === 1) return null;
-
   const pages = range(1, pageCount + 1);
+
   return (
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
           <li
             key={page}
-            className={page === currentPage ? "page-item active" : "page-item"}
+            className={
+              page === Number(currentPage) ? "page-item active" : "page-item"
+            }
           >
-            <a className="page-link" onClick={() => handlePage(page)}>
+            <button
+              className="page-link"
+              onClick={() =>
+                handleSearch(page, currentQuery, currentSort, currentOrder)
+              }
+            >
               {page}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
