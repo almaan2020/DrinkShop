cb46c912c6c9671cc00c8f68d6f1bddd6f5bf186
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-02-14 18:55:39 +0330

    Update FilterTab.jsx

--- src/components/shop/FilterTab.jsx
+++ src/components/shop/FilterTab.jsx
@@ -5,18 +5,26 @@ import filters from "../../config/filters.json";
 
 const FilterTab = (props) => {
   const { filter, label } = props;
-  const { query, handleFilter } = useContext(ProductContext);
+  const { searchParams, handleSearch } = useContext(ProductContext);
+  const {
+    query: currentQuery,
+    sort: currentSort,
+    order: currentOrder,
+  } = Object.fromEntries([...searchParams]);
 
   const { colors } = filters;
 
   const getStyleClass = (filter) => {
-    return query === filter
+    return currentQuery === filter
       ? `nav-link active ${colors.activeColorClass}`
       : `nav-link ${colors.colorClass}`;
   };
 
   return (
-    <li className="nav-item" onClick={() => handleFilter(filter)}>
+    <li
+      className="nav-item"
+      onClick={() => handleSearch(1, filter, currentSort, currentOrder)}
+    >
       <NavLink className={getStyleClass(filter)} to="#">
         {label}
       </NavLink>
