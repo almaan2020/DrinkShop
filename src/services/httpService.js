093694808dec5dd073270ac344841165535a7ebb
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-02-14 18:47:18 +0330

    Update httpService.js

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -16,9 +16,11 @@ axios.interceptors.response.use(null, error => {
   return Promise.reject(error);
 });
 
-export default {
+
+const http = {
   get: axios.get,
   post: axios.post,
   put: axios.put,
   delete: axios.delete
 };
+export default http;
