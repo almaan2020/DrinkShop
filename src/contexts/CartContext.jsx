7f454312ff96811c792bb6f2ec1ec7ff05168c60
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-02-14 18:52:13 +0330

    Update CartContext.jsx

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,50 +1,44 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, useState, useCallback } from "react";
 import { pull } from "lodash";
 
 export const CartContext = createContext();
 
-class CartContextProvider extends Component {
-  state = {
-    cartIds: [],
-  };
+const CartContextProvider = (props) => {
+  const [cartIds, setCartIds] = useState([]);
 
-  fillCartIds = () => {
+  const fillCartIds = useCallback(() => {
     let cartIds = localStorage.getItem("cartProducts");
     //convert localStorage to array of numbers:
     cartIds = cartIds ? cartIds.split(",").map((item) => parseInt(item)) : [];
-    this.setState({ cartIds: cartIds });
-  };
+    setCartIds([...cartIds]);
+  }, [setCartIds]);
 
-  handleCart = (id) => {
-    let cartIds = this.state.cartIds;
+  const handleCart = (id) => {
     if (!cartIds.includes(id)) cartIds.push(id);
     localStorage.removeItem("cartProducts");
     localStorage.setItem("cartProducts", cartIds);
-    this.setState({ cartIds: cartIds });
+    setCartIds([...cartIds]);
   };
 
-  handleRemoveCart = (id) => {
-    let cartIds = this.state.cartIds;
+  const handleRemoveCart = (id) => {
     if (cartIds.includes(id)) pull(cartIds, id);
     localStorage.removeItem("cartProducts");
     localStorage.setItem("cartProducts", cartIds);
-    this.setState({ cartIds: cartIds });
+    setCartIds([...cartIds]);
   };
 
-  render() {
-    return (
-      <CartContext.Provider
-        value={{
-          ...this.state,
-          fillCartIds: this.fillCartIds,
-          handleCart: this.handleCart,
-          handleRemoveCart: this.handleRemoveCart,
-        }}
-      >
-        {this.props.children}
-      </CartContext.Provider>
-    );
-  }
-}
+  return (
+    <CartContext.Provider
+      value={{
+        cartIds,
+        fillCartIds,
+        handleCart,
+        handleRemoveCart,
+      }}
+    >
+      {props.children}
+    </CartContext.Provider>
+  );
+};
 
 export default CartContextProvider;
